1d514c0fc8efd4128093de1ba3826e5801ce17f6
Ghaith <ghaithestaif16@gmail.com>
2025-09-07 19:22:15 +0300

    Initial commit

---
+++ BankSystem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "clsLogInScreen.h"
+//#include "clsLoginRegisterScreen.h"
+
+using namespace std;
+
+
+int main() 
+{
+	while (clsLogInScreen::showLogInScreen());
+	//{
+	//	if (!clsLogInScreen::showLogInScreen()) 
+	//	{
+	//		break;
+	//	}
+	//}
+
+	system("pause > 0");
+	return 0;
+}
